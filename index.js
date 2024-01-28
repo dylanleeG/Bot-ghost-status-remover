76f3d86faebfc8978051d8eb87c6399666fa9afa
dylanleeG <125803953+dylanleeG@users.noreply.github.com>
2024-01-28 21:19:11 +0100

    I wish i never met her at all

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Streaming discord.gg/devouthustlers"];
 
 
 let currentIndex = 0;
